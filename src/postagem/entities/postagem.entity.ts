9debe70de99d7a63826a51eb039ada8de69d7398
Luana <luana.gabriela96@hotmail.com>
2024-07-31 17:08:02 -0300

    security

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,9 +2,11 @@ import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import {Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm"
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
 @Entity ({name: "tb_postagens"})
 export class Postagem {
+    
     @PrimaryGeneratedColumn() //chave primaria Autoincremental
     id : number
 
@@ -26,5 +28,10 @@ export class Postagem {
         onDelete: "CASCADE"
     })
     tema: Tema;
+
+    @ManyToOne(() => Usuario, (usuario) => usuario. postagem, {
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario;
     
 }
